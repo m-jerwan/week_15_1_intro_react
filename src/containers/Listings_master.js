106f64b34d5e3e6410ed118365381d78f95d7699
Marcin Jerwan <m.jerwan@gmail.com>
2018-10-01 20:26:52 +0100

    rendering listings_master correctly

---
+++ src/containers/Listings_master.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Listings_master extends Component{
+
+render(){
+    return(
+        <div>Listings master</div>
+    )
+}
+
+}
+
+export default Listings_master;
